3cbca557f741452c0c9ff0ae2bc0ac9db8219897
Ananya Srivastava <ananyaSrivastavaa9@users.noreply.github.com>
2026-02-09 20:10:46 +0530

    functions added

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int sum (int a, int b) {
+    return a+b;
+}
+int main() {
+    int a, b, sum;
+    a = 10;
+    b = 20;
+    sum = (a+b);
+    cout << "Sum is : " << sum;
+}
