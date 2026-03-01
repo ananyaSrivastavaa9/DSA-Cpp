07ea7a55480a3b6bcacf888db4ff494a2e0df44c
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-02 01:08:50 +0530

    vectors

---
+++ vectors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    // vectors syntax
+
+    // vector <int> vec; //size 0
+    // cout << vec[0] << endl; //segmentation fault
+
+    // vector <int> vec = {1,2,3};
+    // cout << vec[0] << endl;
+    // cout << vec[1] << endl;
+    // cout << vec[2] << endl;
+
+    // vector <int> vec(5,8);
+    // cout << vec[0] << endl;
+    // cout << vec[1] << endl;
+    // cout << vec[2] << endl;
+    // cout << vec[3] << endl;
+    // cout << vec[4] << endl;
+    
+    return 0;
+}
