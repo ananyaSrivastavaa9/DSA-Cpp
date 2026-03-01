d6e816b323a5c386c996fbe5bea585e050c18eac
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-01 20:00:23 +0530

    Array Problems

--- SmallLargeArray.cpp
+++ SmallLargeArray.cpp
@@ -2,23 +2,23 @@
 #include <climits>
 using namespace std;
 int main(){
-    int nums[] = {7,9,74,-8,63,-66,21};
+    int arr[] = {7,9,74,-8,63,-66,21};
     int size = 7;
     int smallest = INT_MAX;
     int largest = INT_MIN;
     // int SmallestIndex = -1; to find index use the comment part
     // int LargestIndex = -1;
     for (int i=0; i < size; i++) {
-        smallest = min(nums[i], smallest);
-        largest = max(nums[i], largest);
-        // if (nums[i] < smallest) {
-        //     smallest = nums[i];
+        // smallest = min(arr[i], smallest);
+        // largest = max(arr[i], largest);
+        if (arr[i] < smallest) {
+            smallest = arr[i];
         //     SmallestIndex = i;
-        // }
-        // if (nums[i] > largest) {
-        //     largest = nums[i];
+        }
+        if (arr[i] > largest) {
+            largest = arr[i];
         //     LargestIndex = i;
-        // }
+        }
     }
     cout << "Smallest is: " << smallest << endl; //SmallestIndex
     cout << "Largest is: " << largest << endl; //LargestIndex
