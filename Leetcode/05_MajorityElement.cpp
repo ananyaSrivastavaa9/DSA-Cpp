ba2aa9ad4a7a2648621cdba9345649e2229325b8
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-13 03:23:53 +0530

    In majority Element (leetcode question) Used 3 methods to solve - Moore's Voting algo, brute force, sorting

--- Leetcode/05_MajorityElement.cpp
+++ Leetcode/05_MajorityElement.cpp
@@ -1,3 +1,59 @@
+// Moore's Voting Algorithm
+// time complexity - O(n)
+
+
+// leetcode solution
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int freq = 0, ans = 0;
+//         int n = nums.size();
+//         for(int i =0; i < n; i++){
+//             if(freq == 0) {
+//                 ans = nums[i];
+//             }
+//             if(ans == nums[i]){
+//                 freq++;
+//             } else {
+//                 freq--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// vs code
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int count = 0;    // to keep track of frequency
+    int candidate = 0; // current potential majority element
+
+    for (int num : nums) {  // loop through each number
+        if (count == 0) {    // if count is 0, pick a new candidate
+            candidate = num;
+        }
+
+        // increase count if same as candidate, else decrease
+        if (num == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;  // the majority element
+}
+
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+    cout << "Majority element is: " << majorityElement(nums) << endl;
+    return 0;
+}
+
+
 // Brute Force Approach
 // Time Complexity - O(n^2)
 // Approach 1
@@ -21,36 +77,36 @@
 //     }
 // };
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        for(int val : nums) {
-            int frequency = 0;
-            for(int el : nums) {
-                if(el == val) {
-                    frequency++;
-                }
-            }
-            if(frequency > n/2) {
-                return val;
-            }
-        }
-        return -1;
-    }
-};
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int val : nums) {
+//             int frequency = 0;
+//             for(int el : nums) {
+//                 if(el == val) {
+//                     frequency++;
+//                 }
+//             }
+//             if(frequency > n/2) {
+//                 return val;
+//             }
+//         }
+//         return -1;
+//     }
+// };
 
-int main() {
-    vector<int> nums = {2,2,1,1,1,2,2};
-    Solution obj;
-    int result = obj.majorityElement(nums);
-    cout << "Majority Element: " << result << endl;
-    return 0;
-}
+// int main() {
+//     vector<int> nums = {2,2,1,1,1,2,2};
+//     Solution obj;
+//     int result = obj.majorityElement(nums);
+//     cout << "Majority Element: " << result << endl;
+//     return 0;
+// }
 
 // Sorting 
 // Time Complexity - O(nlogn)
