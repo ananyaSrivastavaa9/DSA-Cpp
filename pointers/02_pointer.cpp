84f13cea4215248eaa5429679f0408b687cd494c
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-19 01:19:03 +0530

    Add pointer and pointer to pointer demonstration for storing and printing memory addresses

--- pointers/02_pointer.cpp
+++ pointers/02_pointer.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main() {
     int marks = 55;
     int* ptr1 = &marks;
+    int** pptr = &ptr1; // pointer to pointer
+    
     cout << ptr1 << endl; //0x61ff04
     cout << &marks << endl; //0x61ff04
+    cout << &ptr1 << endl; //0x61ff00
+    cout << pptr << endl; //0x61ff00
 
     float price = 67.98;
     float* ptr2 = &price;
-    cout << &price << endl; //0x61ff00
-    cout << ptr2 << endl; //0x61ff00
+    cout << &price << endl; //0x61fefc
+    cout << ptr2 << endl; //0x61fefc
 
     return 0;
 }
