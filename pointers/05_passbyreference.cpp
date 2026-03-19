6888d29fdecfc2fc5bf589bd7dd1b948bc324891
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-19 22:21:08 +0530

    Add pass-by-reference examples using pointers and references in C++

--- pointers/05_passbyreference.cpp
+++ pointers/05_passbyreference.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void ChangeA(int* ptr){ //Pass by reference
+
+// method 1
+void ChangeA(int* ptr){ //Pass by reference using pointers
     *ptr = 20;
 }
 int main(){
@@ -10,3 +12,15 @@ int main(){
     cout << a << endl; //20
     return 0;
 }
+
+// method 2
+
+// void ChangeA(int &b){ //Pass by reference using Alias
+//     b = 20;
+// }
+// int main(){
+//     int a = 10;
+//     ChangeA(a);
+//     cout << a << endl; //20
+//     return 0;
+// }
