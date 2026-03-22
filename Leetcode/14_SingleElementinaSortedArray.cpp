27e020d96af9eb50cd490f6ba21c93fca3f8a774
Ananya Srivastava <your_github_username@users.noreply.github.com>
2026-03-22 06:38:58 +0530

    feat: implement binary search solution for single non-duplicate element (LeetCode 540)

--- Leetcode/14_SingleElementinaSortedArray.cpp
+++ Leetcode/14_SingleElementinaSortedArray.cpp
@@ -31,3 +31,106 @@ int main() {
     cout << SE(arr) << endl;
     return 0;
 }
+
+// optimised approach
+// time complexity - 0(n)
+// space complexity - 0(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        int st = 0;
+        int end = n - 1;
+
+        if(n == 1) {
+            return arr[0];
+        }
+
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if(mid == 0 && arr[0] != arr[mid+1]) {
+                return arr[mid];
+            }
+            if(mid == n-1 && arr[n-1] != arr[n-2]) {
+                return arr[mid];
+            }
+
+            if(arr[mid] != arr[mid - 1] && arr[mid] != arr[mid+1]) {
+                return arr[mid];
+            }
+
+            if (mid % 2 == 0) {
+                if(arr[mid] == arr[mid+1]) {
+                    st = mid + 2;
+                } else {
+                    end = mid - 1;
+                }
+            } 
+            else {
+                if(arr[mid-1] == arr[mid]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> arr = {1,1,2,2,3,4,4}; // test case
+
+    cout << obj.singleNonDuplicate(arr) << endl;
+
+    return 0;
+}
+
+// leetcode
+
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& arr) {
+//         int n = arr.size();
+//         int st =0;
+//         int end = n - 1;
+//         if(n==1) {
+//             return arr[0];
+//         }
+//         while(st <= end) {
+//             int mid = st + (end - st) / 2;
+//             if(mid == 0 && arr[0] != arr[mid+1]) {
+//                 return arr[mid];
+//             }
+//             if(mid == n-1 && arr[n-1] != arr[n-2]) {
+//                 return arr[mid];
+//             }
+//             if(arr[mid] != arr[mid - 1] && arr[mid] != arr[mid+1]) {
+//                 return arr[mid];
+//             }
+//             if (mid % 2 == 0) {
+//                 if(arr[mid] == arr[mid+1]) {
+//                     st = mid + 2;
+//                 } else {
+//                     end = mid - 1;
+//                 }
+//             }
+//             if (mid % 2 != 0) {
+//                 if(arr[mid-1] == arr[mid]) {
+//                     st = mid+1;
+//                 } else {
+//                     end = mid -1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
