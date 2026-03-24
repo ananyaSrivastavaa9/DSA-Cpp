9ceb056851dd6ac25de8104ac9d43c0abd7e78dd
Ananya Srivastava <your_github_username@users.noreply.github.com>
2026-03-25 03:37:22 +0530

    Painter's Partition Problem

--- Leetcode/16_PainterPartition.cpp
+++ Leetcode/16_PainterPartition.cpp
@@ -1,31 +1,56 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
-void is_possible(vector<int> arr, int m, int n, int maxtime) {
-
+bool is_possible(vector<int> &arr, int n, int m, int maxallowedtime) {
+    int painters = 1;
+    int time = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i]>maxallowedtime) {
+            return false;
+        }
+        if (time + arr[i] <= maxallowedtime) {
+            time += arr[i];
+        } else {
+            painters++;
+            time = arr[i];
+        }
+    }
+    if (painters<= m) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-
 int PainterPartition(vector<int> &arr, int n, int m) {
+    int sum = 0, maxVal = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+    int st = maxVal;
+    int end = sum;
+    int ans = -1;
     if (m > n) {
         return -1;
     }
-    int sum = 0;
-    for (int i = 0; i < n; i ++) {
-        sum += arr[i];
-    }
-    int n = arr.size();
-    int st = 0;
-    int end = n - 1;
-    int ans = -1;
-    while(st <= end) {
-        int mid = st + (end - st)/2;
-        if (is_possible(arr, n, m, mid)) {
-            ans = mid
+    while(st<=end) {
+        int mid = st + (end - st) / 2;
+        if (is_possible(arr,n,m,mid)) {
+            ans = mid;
             end = mid - 1;
         } else {
             st = mid + 1;
         }
     }
     return ans;
+}
+
+int main() {
+    vector<int>arr = {40,30,10,20};
+    int n = 4;
+    int m = 2;
+    cout << PainterPartition(arr,n,m) << endl;
+    return 0;
 }
