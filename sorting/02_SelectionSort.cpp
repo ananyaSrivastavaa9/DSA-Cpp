ae577b4cb8d721c72b7f0d7abd63120243b43663
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-27 05:12:38 +0530

    Selection Sorting Optimised Code

---
+++ sorting/02_SelectionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+void selectionsort(int arr[], int n) {
+    bool is_swap = false;
+    for(int i = 0; i < n - 1; i++) {
+        int SmallestElement = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[SmallestElement]) {
+                SmallestElement = j;
+            }
+        }
+        swap(arr[i], arr[SmallestElement]);
+        is_swap = true;
+    }
+}
+int printarr(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int arr[] = {4,1,5,2,3};
+    int n = 5;
+    selectionsort(arr,n);
+    printarr(arr,n);
+    return 0;
+}
