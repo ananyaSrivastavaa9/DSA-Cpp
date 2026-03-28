28d3a4412ec71f7b509ba740dbe1886b1d2b96ca
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-03-28 16:13:13 +0530

    Sort Colors - DUTCH NATIONAL FLAG ALGORITHM ADDED

--- Leetcode/18_sortcolor.cpp
+++ Leetcode/18_sortcolor.cpp
@@ -1,22 +1,90 @@
 // leetcode - 75
 
+// optimised approach - DUTCH NATIONAL FLAG ALGORITHM
+// time complexity - O(n)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    Solution obj;
+    obj.sortColors(nums);
+
+    cout << "Sorted array: ";
+    for(int x : nums) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int low = 0, mid = 0, high = nums.size() - 1;
+
+//         while(mid <= high) {
+//             if(nums[mid] == 0) {
+//                 swap(nums[low], nums[mid]);
+//                 low++;
+//                 mid++;
+//             }
+//             else if(nums[mid] == 1) {
+//                 mid++;
+//             }
+//             else { // nums[mid] == 2
+//                 swap(nums[mid], nums[high]);
+//                 high--;
+//             }
+//         }
+//     }
+// };
+
+
 // brute force approach
+
 // time complexity - O(nlogn)
 // space complexity - O(1)
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-void SortColors(vector<int>arr, int n) {
-    sort(arr.begin(), arr.end());
-    for(int i = 0; i < n; i++) {
-        cout << arr[i];
-    }
-}
-int main() {
-    vector<int>arr = {2,0,2,1,1,0,1,2,0,0};
-    int n = 10;
-    SortColors(arr,n);
-    return 0;
-}
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// void SortColors(vector<int>arr, int n) {
+//     sort(arr.begin(), arr.end());
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i];
+//     }
+// }
+// int main() {
+//     vector<int>arr = {2,0,2,1,1,0,1,2,0,0};
+//     int n = 10;
+//     SortColors(arr,n);
+//     return 0;
+// }
