dd44969c9195071b4919b799c4bf210ffabd9208
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-04-14 19:29:23 +0530

    Stack Added

--- STL/13_stack.cpp
+++ STL/13_stack.cpp
@@ -4,17 +4,33 @@
 using namespace std;
 int main() {
     stack<int>s;
+
+    // push() function / emplace() you can use
     s.push(1);
     s.push(2);
     s.push(3);
-    
-    // to access the top element
+
+    stack<int>s2;
+
+    // to swap the elements of one stack to another
+    // s2.swap(s);
+
+    // to check the size of the stack - size()
+    cout << "Size of s stack: " << s.size() << endl;
+    cout << "Size of s2 stack: " << s2.size() << endl;
+
+    // to access the top element - top()
     cout << "Top element is: " << s.top() << endl;
+
+    // empty() to check if the stack is empty or not 
 
     while(!s.empty()) {
         cout << s.top() << " ";
+
+        // to pop out the element (top one) from the stack
         s.pop();
     }
+
     cout << endl;
     return 0;
 }
