2cf9c8d922a193e526357bffa53496d07860d6ed
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-04-14 05:42:43 +0530

    Stack Added

---
+++ STL/13_stack.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main() {
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    
+    // to access the top element
+    cout << "Top element is: " << s.top() << endl;
+
+    while(!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+    return 0;
+}
