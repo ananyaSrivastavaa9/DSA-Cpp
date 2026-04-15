325603f94da53099ea87644ff508269126894f88
Ananya Srivastava <ananyasrivastavaa9@gmail.com>
2026-04-16 00:16:06 +0530

    Strings

---
+++ Strings/05_strings.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string str = "Ananya";
+    cout << str << endl;
+    str = "hello"; // dynamic = runtime resize
+    cout << str << endl;
+    return 0;
+}
